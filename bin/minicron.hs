fe2fcd11bb0a817f319fc298060b853f3f0a4cd9
Vo Minh Thu <thu@hypered.io>
2014-11-24 17:34:41 +0100

    Remove unused imports.

--- bin/minicron.hs
+++ bin/minicron.hs
@@ -1,17 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
-import Control.Concurrent (forkIO, killThread, threadDelay, ThreadId)
-import Control.Concurrent.Chan (newChan, readChan, writeChan, Chan)
-import Control.Concurrent.MVar (modifyMVar, newMVar, takeMVar, putMVar, MVar)
-import Data.AffineSpace ((.+^), (.-.))
-import Data.List (sortBy)
-import Data.Ord (comparing)
-import Data.Text (Text)
-import qualified Data.Text as T
-import Data.Thyme.Clock (fromSeconds, getCurrentTime, toSeconds, UTCTime)
-import Data.Thyme.Format (formatTime)
-import System.Locale (defaultTimeLocale, iso8601DateFormat)
+import Data.AffineSpace ((.+^))
+import Data.Thyme.Clock (fromSeconds, getCurrentTime)
 
 import System.Cron.Mini
 
