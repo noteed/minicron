9d5742a119f0393b5821c68160378848f56eada5
Vo Minh Thu <thu@hypered.io>
2017-04-08 20:39:54 +0200

    Support GHC 7.10.3.

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env bash
 
+STACK_IMAGE=${1:-7.8.4}
+
 docker run \
   -v `pwd`:/source \
-  -t -i images.reesd.com/reesd/stack \
-  sh -c 'cd /source ; cabal install'
+  -t -i images.reesd.com/reesd/stack:$STACK_IMAGE \
+  sh -c 'cd /source ; ghc --version ; cabal install'
